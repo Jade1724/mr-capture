15995f3e57661f542fd82d7b253b58021044e0ef
Jade1724 <haruka.i1724@gmail.com>
2022-05-02 18:55:59 +1200

    Ajusted the hsv range to lab's blue screen

--- main.py
+++ main.py
@@ -6,9 +6,29 @@ video = VideoCapture(0)
 
 while True:
     ret, frame = video.read()
-    cv2.imshow('Frame', frame)
+
+    # Convert BGR image to HSV image
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Define the background as a blue color in the following range
+    lower_blue = np.array([32, 94, 70])
+    upper_blue   = np.array([179, 255, 255])
+    
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    cv2.imshow('Frame', mask)
     if cv2.waitKey(1) == ord('q'):
         break
+
+
+    # dark blue
+    # h 242
+    # s 94
+    # v 60
+
+    # lighter blue 
+    # h 242
+    # s 83
+    # v 100
 
 video.release()
 cv2.destroyAllWindows()
