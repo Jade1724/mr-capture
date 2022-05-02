e458643bb8032bf7085c7511a7a618f1a9cd61c0
Jade1724 <haruka.i1724@gmail.com>
2022-05-02 19:44:40 +1200

    Tested video overlay on another video

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import cv2
 import numpy as np
 
+FRAME_SIZE = (960, 720)
 video = cv2.VideoCapture(0)
-image = cv2.imread('assets/ocean.jpg')
-image = cv2.resize(image, (640, 480))
+bg_video = cv2.VideoCapture('assets/robot-dance-floor.mp4')
 
 
 while True:
     ret, frame = video.read()
+    frame = cv2.resize(frame, FRAME_SIZE)
+    bg_ret, bg_frame = bg_video.read()
+    bg_frame = cv2.resize(bg_frame, FRAME_SIZE)
 
     # Convert BGR image to HSV image
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -19,7 +22,7 @@ while True:
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
     res = cv2.bitwise_and(frame, frame, mask=mask)
     bg_mask = frame - res
-    blue_screen = np.where(bg_mask==0, image, bg_mask)
+    blue_screen = np.where(bg_mask==0, bg_frame, bg_mask)
 
     cv2.imshow('Frame', blue_screen)
     if cv2.waitKey(1) == ord('q'):
